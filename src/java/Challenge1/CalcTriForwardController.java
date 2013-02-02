77938b06d7df63d5a6ece86695b09cb1d74f261e
jgray2@my.wctc.edu <>
2013-02-01 19:56:02 -0600

    add separate controllers for each calculation

--- src/java/Challenge1/CalcTriForwardController.java
+++ src/java/Challenge1/CalcTriForwardController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,20 +13,12 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Jenni Burgmeier
  */
-public class CalculationForwardController extends HttpServlet {
+@WebServlet(name = "CalcTriForwardController", urlPatterns = {"/TriController.do"})
+public class CalcTriForwardController extends HttpServlet {
 
     private static final String destination = "/result.jsp";
-    private String rlegA;
-    private String rlegB;
-    private String rArea;
-    private String radius;
-    private double pi = 3.14159265359;
-    private String cArea;
-    private String tLegA;
-    private String tLegB;
-    private String tLegC;
-
-    /**
+    
+     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
      * <code>POST</code> methods.
@@ -39,19 +32,14 @@ public class CalculationForwardController extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-
-        rlegA = request.getParameter("rlegA");
-        rlegB = request.getParameter("rlegB");
-        calculateRectangleArea();
-        request.setAttribute("rArea", rArea);
-
-        radius = request.getParameter("radius");
-        calculateCircleArea();
-        request.setAttribute("cArea", cArea);
+       
+        String tLegA = request.getParameter("tLegA");
+        String tLegB = request.getParameter("tLegB");
+        double length;
+        length = Math.sqrt((Double.parseDouble(tLegA) * Double.parseDouble(tLegA) *
+                (Double.parseDouble(tLegB) * Double.parseDouble(tLegB))));
+        String tLegC = String.valueOf(length);
         
-        tLegA = request.getParameter("tLegA");
-        tLegB = request.getParameter("tLegB");
-        calculateTriangleLeg();
         request.setAttribute("tLegC", tLegC);
 
         RequestDispatcher dispatcher =
@@ -59,24 +47,7 @@ public class CalculationForwardController extends HttpServlet {
         dispatcher.forward(request, response);
     }
 
-    private void calculateRectangleArea() {
-        double area;
-        area = Double.parseDouble(rlegA) * Double.parseDouble(rlegB);
-        rArea = String.valueOf(area);
-    }
-    
-    private void calculateCircleArea() {
-        double area;
-        area = Double.parseDouble(radius)* Double.parseDouble(radius) * pi ;
-        cArea = String.valueOf(area);
-    }
-    
-    private void calculateTriangleLeg() {
-        double length;
-        length = Math.sqrt((Double.parseDouble(tLegA) * Double.parseDouble(tLegA) *
-                (Double.parseDouble(tLegB) * Double.parseDouble(tLegB))));
-        tLegC = String.valueOf(length);
-    }
+  
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
